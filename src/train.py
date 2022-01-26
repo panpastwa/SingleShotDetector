e0a3e9e83caf561a0932a6afd34b4ea65f75dca7
Marcin Pastwa <marpas001@gmail.com>
2022-01-26 12:26:45 +0100

    Improve training loop, add training progress charts

--- src/train.py
+++ src/train.py
@@ -19,9 +19,10 @@ ssd.to(DEVICE)
 
 optimizer = torch.optim.SGD(params=ssd.parameters(), lr=0.001)
 
-iterator = tqdm(dataloader)
-for epoch in range(5):
+mean_losses = []
+for epoch in range(3):
     losses = []
+    iterator = tqdm(dataloader)
     for i, (images, targets) in enumerate(iterator):
         images = images.to(DEVICE)
         loss = ssd(images, targets)
@@ -29,10 +30,24 @@ for epoch in range(5):
         optimizer.step()
         iterator.set_description(f"Loss: {loss:.2f}")
         losses.append(loss)
+        if i == 10:
+            break
 
     torch.save(ssd.state_dict(), f"../weights/weights{epoch+1}")
+
     losses = torch.tensor(losses).cpu()
+    mean_losses.append(losses.mean())
+
+    plt.figure()
     plt.plot(losses)
+    plt.title(f"Epoch {epoch+1:2d}")
     plt.xlabel("Iteration")
     plt.ylabel("Loss")
-    plt.show()
+    plt.savefig(f"../figures/training_epoch_{epoch+1:2d}")
+
+plt.figure()
+plt.plot(mean_losses)
+plt.title(f"Training curve")
+plt.xlabel("Epoch")
+plt.ylabel("Mean loss")
+plt.savefig("../figures/training_curve.png")
